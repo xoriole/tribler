22156e75bc84c950f94803031dba9000404d3400
Sandip Pandey <np.sandip.pandey@gmail.com>
2019-01-23 10:19:01 +0100

    Fixed a bug on token mining graph

--- TriblerGUI/widgets/tokenminingpage.py
+++ TriblerGUI/widgets/tokenminingpage.py
@@ -170,8 +170,9 @@ class TokenMiningPage(QWidget):
     def push_data_to_plot(self, upload, download):
         # Keep only last 100 records to show in graph
         if len(self.plot_data[1]) > 100:
-            self.plot_data[0] = self.plot_data[0][-100:]
-            self.plot_data[1] = self.plot_data[1][-100:]
+            self.plot_data[1] = self.plot_data[0][0][-100:]
+            self.plot_data[0][0] = self.plot_data[0][0][-100:]
+            self.plot_data[0][1] = self.plot_data[0][1][-100:]
 
         self.plot_data[1].append(datetime.datetime.now())
         self.plot_data[0][0].append(upload / self.byte_scale)
