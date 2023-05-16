cb69174f0e9cbea2c0fc702b1d3fd609b7b2b22c
Sandip Pandey <np.sandip.pandey@gmail.com>
2023-05-16 12:20:58 +0200

    Update http tracker and its tests

--- src/tribler/core/components/torrent_checker/torrent_checker/trackers/http.py
+++ src/tribler/core/components/torrent_checker/torrent_checker/trackers/http.py
@@ -1,7 +1,7 @@
 import logging
 import time
 from asyncio.exceptions import TimeoutError
-from typing import List
+from typing import List, Dict, Tuple, Optional
 
 import async_timeout
 from aiohttp import ClientSession, ClientTimeout, ClientResponseError
@@ -14,30 +14,106 @@ from tribler.core.components.torrent_checker.torrent_checker.trackers.exceptions
 from tribler.core.utilities.tracker_utils import add_url_params, parse_tracker_url
 from tribler.core.utilities.utilities import bdecode_compat
 
+# Keys found in tracker response dictionary
+KEY_FILES = b'files'
+KEY_FAILURE_REASON = b'failure reason'
+KEY_COMPLETE = b'complete'
+KEY_INCOMPLETE = b'incomplete'
+
 
 class HttpTracker(Tracker):
+    """
+    HTTP Tracker with support for using proxy for anonymized requests.
+    It supports fetching tracker response from both HTTP and HTTPS trackers.
 
-    def __init__(self, proxy=None):
+    Usage:
+
+    # Without Proxy
+    http_tracker = HttpTracker()
+
+    # With Proxy
+    https_tracker = HttpTracker(proxy='localhost:5000')
+
+    # To get torrent health info
+    torrent_health_info = await http_tracker.get_torrent_health(b'infohash', 'https://sometracker.com', timeout=5)
+    """
+
+    def __init__(self, proxy: Optional[str] = None):
+        """
+        Args:
+            proxy: Optional proxy to use for http request. eg. localhost:5000
+        """
         self._logger = logging.getLogger(self.__class__.__name__)
         self.proxy_connector = self.get_proxy_connector(proxy)
 
-    def is_supported_url(self, tracker_url: str):
+    def is_supported_url(self, tracker_url: str) -> bool:
+        """
+        Return True if the tracker_url is supported.
+
+        Args:
+            tracker_url: Tracker URL.
+
+        Returns:
+            True: If the tracker URL starts with http
+            False: Otherwise
+        """
         return tracker_url.lower().startswith("http")
 
-    async def get_torrent_health(self, infohash, tracker_url, timeout=5):
+    async def get_torrent_health(
+            self,
+            infohash: bytes,
+            tracker_url: str,
+            timeout: Optional[int] = 5
+    ) -> Optional[HealthInfo]:
+        """
+        Get torrent health info for the given torrent from the given tracker.
+
+        Args:
+            infohash: Torrent infohash
+            tracker_url: Tracker URL. Could be http or https URL.
+            timeout: Timeout in seconds.
+
+        Raises:
+            TrackerException: If there is failure in retrieving the tracker response
+
+        Returns:
+            HealthInfo : HealthInfo of the torrent including seeders and leechers if the request is successful.
+                         None otherwise.
+        """
         tracker_response: TrackerResponse = await self.get_tracker_response(tracker_url, [infohash], timeout=timeout)
         if tracker_response and tracker_response.torrent_health_list:
             return tracker_response.torrent_health_list[0]
         return None
 
-    async def get_tracker_response(self, tracker_url, infohashes, timeout=5) -> TrackerResponse:
-        scrape_url = self.get_scrape_url(tracker_url, infohashes)
+    async def get_tracker_response(
+            self,
+            tracker_url: str,
+            infohashes: List[bytes],
+            timeout: Optional[int] = 5
+    ) -> TrackerResponse:
+        """
+        Return Tracker response for a given tracker URL with a list of infohashes.
 
+        Args:
+            tracker_url: Tracker URL.
+            infohashes: List of torrent infohashes (bytes)
+            timeout: Timeout in seconds to wait for a response.
+
+        Returns:
+            TrackerResponse: TrackerResponse with torrent health info of included torrents.
+
+        Raises:
+            TimeoutException: If the response was not received from the tracker within time.
+            InvalidTrackerURL: If encoding the tracker URL raises UnicodeEncodeError.
+            InvalidHttpResponse: If HTTP Response other than HTTP_OK (status_code:200) is returned by the tracker.
+            TrackerException: For all other exceptions.
+        """
         try:
             async with async_timeout.timeout(timeout):
-                session = self.get_http_session(timeout)
-                tracker_response = await self.get_http_response(session, scrape_url)
-                health_list = self.process_tracker_response(tracker_response)
+                session = self._get_http_session(timeout)
+                scrape_url = self.get_scrape_url(tracker_url, infohashes)
+                raw_response = await self.get_http_response(session, scrape_url)
+                health_list = self.process_tracker_response(raw_response)
                 return TrackerResponse(url=tracker_url, torrent_health_list=health_list)
 
         except TimeoutError as ex:
@@ -50,7 +126,21 @@ class HttpTracker(Tracker):
             raise TrackerException() from other_exceptions
 
     @staticmethod
-    def get_scrape_url(tracker_url, infohashes):
+    def get_scrape_url(tracker_url: str, infohashes: List[bytes]) -> str:
+        """
+        Get proper scrape URL from the tracker announce URL and list of torrent infohashes.
+
+        Args:
+            tracker_url: Tracker URL. Usually ends with /announce.
+            infohashes: List of torrent infohashes.
+
+        Returns:
+            A properly encoded scrape URL.
+
+        Raises:
+            MalformedTrackerURLException: If the tracker URL cannot be parsed properly.
+            UnicodeEncodeError: If there is exception on encoding the URL.
+        """
         tracker_type, tracker_address, announce_page = parse_tracker_url(tracker_url)
 
         scrape_page = announce_page.replace('announce', 'scrape')
@@ -61,23 +151,23 @@ class HttpTracker(Tracker):
         scrape_url = add_url_params(scrape_url, {"info_hash": infohashes})
         return scrape_url
 
-    def get_http_session(self, timeout):
+    def _get_http_session(self, timeout: int) -> ClientSession:
         return ClientSession(connector=self.proxy_connector,
                              raise_for_status=True,
                              timeout=ClientTimeout(total=timeout))
 
     @staticmethod
-    def get_proxy_connector(proxy):
+    def get_proxy_connector(proxy: Optional[str]) -> Socks5Connector:
         return Socks5Connector(proxy) if proxy else None
 
     @staticmethod
-    async def get_http_response(session, url):
+    async def get_http_response(session: ClientSession, url: str) -> Optional[bytes]:
         async with session:
             async with session.get(url) as response:
                 return await response.read()
 
     @staticmethod
-    def process_tracker_response(body) -> List[HealthInfo]:
+    def process_tracker_response(body: Optional[bytes]) -> List[HealthInfo]:
         if body is None:
             raise EmptyResponseException()
 
@@ -85,10 +175,10 @@ class HttpTracker(Tracker):
         if not response_dict:
             raise InvalidResponseException()
 
-        if b'failure reason' in response_dict:
-            raise ExceptionWithReason(response_dict[b'failure reason'])
+        if KEY_FAILURE_REASON in response_dict:
+            raise ExceptionWithReason(response_dict[KEY_FAILURE_REASON])
 
-        files = response_dict.get(b'files')
+        files = response_dict.get(KEY_FILES)
         if not isinstance(files, dict):
             return []
 
@@ -96,18 +186,18 @@ class HttpTracker(Tracker):
         now = int(time.time())
 
         for infohash, file_info in files.items():
-            seeders, leechers = HttpTracker.get_health_from_file_info(file_info)
+            seeders, leechers = HttpTracker.parse_file_info(file_info)
             healthinfo = HealthInfo(infohash, last_check=now, seeders=seeders, leechers=leechers, self_checked=True)
             health_list.append(healthinfo)
 
         return health_list
 
     @staticmethod
-    def get_health_from_file_info(file_info):
+    def parse_file_info(file_info: Dict) -> Tuple[int, int]:
         seeders = leechers = 0
         if isinstance(file_info, dict):
             # "complete: number of peers with the entire file, i.e. seeders (integer)"
             #  - https://wiki.theory.org/BitTorrentSpecification#Tracker_.27scrape.27_Convention
-            seeders = file_info.get(b'complete', 0)
-            leechers = file_info.get(b'incomplete', 0)
+            seeders = file_info.get(KEY_COMPLETE, 0)
+            leechers = file_info.get(KEY_INCOMPLETE, 0)
         return seeders, leechers
--- src/tribler/core/components/torrent_checker/torrent_checker/trackers/tests/test_http.py
+++ src/tribler/core/components/torrent_checker/torrent_checker/trackers/tests/test_http.py
@@ -1,9 +1,13 @@
 from binascii import unhexlify
-from unittest.mock import patch, AsyncMock
+from unittest.mock import patch, AsyncMock, Mock
 
 import pytest
+from aiohttp import ClientResponseError
 
+from tribler.core.components.torrent_checker.torrent_checker.dataclasses import HealthInfo
 from tribler.core.components.torrent_checker.torrent_checker.trackers import TrackerException
+from tribler.core.components.torrent_checker.torrent_checker.trackers.exceptions import EmptyResponseException, \
+    InvalidResponseException, InvalidHttpResponse, InvalidTrackerURL
 from tribler.core.components.torrent_checker.torrent_checker.trackers.http import HttpTracker
 
 SAMPLE_INFOHASH = unhexlify("2c6b6858d61da9543d4231a71db4b1c9264b0685")  # Ubuntu 20.04
@@ -13,15 +17,66 @@ SAMPLE_INVALID_TRACKER_RESPONSE = b'd8:announce36:http://tracker.example.com/inv
                                   b':failure reason23:invalid announce url - 1e'
 
 
+@pytest.fixture
+def http_tracker():
+    return HttpTracker()
+
+
+def test_is_supported_url(http_tracker):
+    assert http_tracker.is_supported_url("http://example.com")
+    assert not http_tracker.is_supported_url("udp://example.com")
+
+
+def test_get_scrape_url(http_tracker):
+    tracker_url = "http://example.com/announce"
+    infohashes = ["0123456789abcdef"]
+
+    with patch("tribler.core.utilities.tracker_utils.parse_tracker_url",
+               return_value=("http", ("example.com", "80"), "/announce")):
+        result = http_tracker.get_scrape_url(tracker_url, infohashes)
+
+    assert result == "http://example.com:80/scrape?info_hash=0123456789abcdef"
+
+
+def test_get_proxy_connector_with_proxy(http_tracker):
+    proxy = "socks5://127.0.0.1:9050"
+    connector = http_tracker.get_proxy_connector(proxy)
+    assert connector is not None
+
+
+def test_get_proxy_connector_without_proxy(http_tracker):
+    connector = http_tracker.get_proxy_connector(None)
+    assert connector is None
+
+
 async def test_get_tracker_response():
     tracker_url = 'http://tracker.example.com/announce'
 
-    with patch('aiohttp.ClientSession') as mock_session:
-        http_tracker = HttpTracker()
-        http_tracker.get_http_response = AsyncMock(return_value=SAMPLE_VALID_TRACKER_RESPONSE)
-        response = await http_tracker.get_tracker_response(tracker_url, [SAMPLE_INFOHASH], timeout=0.01)
-        assert response.url == tracker_url
-        assert len(response.torrent_health_list) == 1
+    # with patch('aiohttp.ClientSession') as mock_session:
+    http_tracker = HttpTracker()
+    http_tracker._get_http_session = lambda timeout: Mock()
+
+    # Test success response
+    http_tracker.get_http_response = AsyncMock(return_value=SAMPLE_VALID_TRACKER_RESPONSE)
+    response = await http_tracker.get_tracker_response(tracker_url, [SAMPLE_INFOHASH], timeout=0.01)
+    assert response.url == tracker_url
+    assert len(response.torrent_health_list) == 1
+
+    # Test HTTP error response
+    http_error = ClientResponseError(Mock(), Mock())
+    http_tracker.get_http_response = AsyncMock(side_effect=http_error)
+    with pytest.raises(InvalidHttpResponse):
+        _ = await http_tracker.get_tracker_response(tracker_url, [SAMPLE_INFOHASH], timeout=0.01)
+
+    # Unicode error on scrape URL
+    def mock_get_scrape_url(*args, **kwargs):
+        url = "http://someunicodecharintracker.org"
+        raise UnicodeEncodeError('utf-8', url, 1, 2, 'Fake Unicode Error!')
+
+    http_tracker.get_scrape_url = Mock(side_effect=mock_get_scrape_url)
+    http_tracker.get_http_response = AsyncMock(return_value=SAMPLE_VALID_TRACKER_RESPONSE)
+    with pytest.raises(InvalidTrackerURL):
+        _ = await http_tracker.get_tracker_response(tracker_url, [SAMPLE_INFOHASH], timeout=0.01)
 
 
 def test_process_body_invalid_response():
